66221fe8afd7e9828c48f2c4c6b1aee398edd25c
Ilya Selenzev <blizardsis@yandex.ru>
2023-10-25 17:17:06 +0300

    database1

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
+    description: str = 'Приложение по бронированию переговорок'
+    database_url: str
 
     class Config:
         env_file = '.env'
--- app/main.py
+++ app/main.py
@@ -3,4 +3,7 @@ from fastapi import FastAPI
 from app.core.config import settings
 
 
-app = FastAPI(title=settings.app_title)
+app = FastAPI(
+    title=settings.app_title,
+    description=settings.description
+)
