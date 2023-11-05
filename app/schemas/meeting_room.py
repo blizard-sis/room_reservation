8354bb2b079a0d8f34f5cbd7b5566ef3db8f08ec
Ilya Selenzev <blizardsis@yandex.ru>
2023-11-05 20:49:31 +0300

    1

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -1,12 +1,21 @@
+# app/schemas/meeting_room.py
+
 from typing import Optional
 
 from pydantic import BaseModel, Field
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(
-        ...,
-        max_length=100,
-        title='Нзвание',
-    )
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
     description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(..., min_length=1, max_length=100)
+
+
+class MeetingRoomDB(MeetingRoomCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
