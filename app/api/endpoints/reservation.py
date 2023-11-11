c03918eec6bbc7049363dae4ca0d94ea200d25c2
Ilya Selenzev <blizardsis@yandex.ru>
2023-11-11 21:07:38 +0300

    finish reservations

--- app/api/endpoints/reservation.py
+++ app/api/endpoints/reservation.py
@@ -8,7 +8,9 @@ from app.api.validators import (
 )
 from app.core.db import get_async_session
 from app.crud.reservation import reservation_crud
-from app.schemas.reservation import ReservationCreate, ReservationDB
+from app.schemas.reservation import (
+    ReservationCreate, ReservationDB, ReservationUpdate
+)
 
 router = APIRouter()
 
@@ -36,6 +38,44 @@ async def get_all_reservations(
 ):
     reservations = await reservation_crud.get_multi(session)
     return reservations
+
+
+@router.get(
+    '/{meeting_room_id}/reservations',
+    response_model=list[ReservationDB]
+)
+async def get_reservations_for_room(
+        meeting_room_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    await check_meeting_room_exists(meeting_room_id, session)
+    reservations = await reservation_crud.get_future_reservations_for_room(
+        room_id=meeting_room_id, session=session
+    )
+    return reservations
+
+
+@router.patch('/{reservation_id}', response_model=ReservationDB)
+async def update_reservation(
+        reservation_id: int,
+        obj_in: ReservationUpdate,
+        session: AsyncSession = Depends(get_async_session),
+):
+    reservation = await check_reservation_before_edit(
+        reservation_id, session
+    )
+    await check_reservation_intersections(
+        **obj_in.dict(),
+        reservation_id=reservation_id,
+        meetingroom_id=reservation.meetingroom_id,
+        session=session
+    )
+    reservation = await reservation_crud.update(
+        db_obj=reservation,
+        obj_in=obj_in,
+        session=session,
+    )
+    return reservation
 
 
 @router.delete('/{reservation_id}', response_model=ReservationDB)
