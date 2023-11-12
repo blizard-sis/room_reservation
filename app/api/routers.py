b69a98c5af18f9ca3d0a4205e0c672bac859d94c
Ilya Selenzev <blizardsis@yandex.ru>
2023-11-12 17:16:28 +0300

    finish users

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import meeting_room_router, reservation_router
+from app.api.endpoints import (
+    meeting_room_router, reservation_router, user_router
+)
 
 main_router = APIRouter()
 main_router.include_router(
@@ -12,4 +14,7 @@ main_router.include_router(
     reservation_router,
     prefix='/reservations',
     tags=['Reservations']
+)
+main_router.include_router(
+    user_router
 )
--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     description: str = 'Приложение по бронированию переговорок'
     database_url: str
+    secret: str = 'SECRET'
 
     class Config:
         env_file = '.env'
